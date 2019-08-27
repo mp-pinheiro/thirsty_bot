e3025e83b800abef498c35484ef441cc730dc8b4
Matheus Pinheiro <mp-pinheiro@hotmail.com>
2019-08-27 16:17:40 -0300

    Initial files

---
+++ index.js
@@ -0,0 +1,49 @@
+const TwitchBot = require('twitch-bot')
+
+var api = require('twitch-api-v5');
+
+api.clientID = '48ft52vl3ve9sugb0wxyt7qzjer2j4';
+api.debug = true;
+
+// api.users.usersByName({ users: ['starcat'] }, (err, res) => {
+//   if(err) {
+//       console.log(err);
+//   } else {
+//       console.log(res);
+//   }
+// });
+
+// console.log(api.streams.channel)
+
+api.search.streams({query: 'starcat'}, (err, res) => {
+  if(err) {
+      console.log(err);
+  } else {
+      console.log(res);
+  }
+});
+ 
+const Bot = new TwitchBot({
+  username: 'fairfruitbot',
+  oauth: 'oauth:***REMOVED***',
+  channels: ['fairfruit']
+})
+ 
+Bot.on('join', channel => {
+  console.log(`Joined channel: ${channel}`)
+})
+ 
+Bot.on('error', err => {
+  console.log(err)
+})
+ 
+Bot.on('message', chatter => {
+  if(chatter.message === '!test') {
+    Bot.say('Command executed! PogChamp')
+  }
+})
+
+var minutes = 1, the_interval = minutes * 60 * 1000;
+setInterval(function() {
+    Bot.say("It's been 5 minutes!")
+}, the_interval);
